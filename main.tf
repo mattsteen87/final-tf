f1593006c741a6b46a238abd747b9441cc5cecb2
Matt <msteen@2ndwatch.com>
2022-04-15 10:57:30 -0700

    added subnet

--- main.tf
+++ main.tf
@@ -17,6 +17,20 @@ backend "s3" {
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_vpc" "new-cow" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "cowwww"
+  }
+  resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.new-cow.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
+}
 resource "aws_instance" "car" {
   ami           = "ami-03ededff12e34e59e"
   instance_type = "t2.micro"
