72c169adaa3fee928d17153cd3211d9af1afb6af
mattsteen87 <msteen@2ndwatch.com>
2022-04-14 17:39:59 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+backend "s3" {
+     bucket         = "msteentest-tf-state" # REPLACE WITH YOUR BUCKET NAME
+     key            = "terraform.tfstate"
+     region         = "us-east-1"
+    #dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+   }
+     required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "car" {
+  ami           = "ami-03ededff12e34e59e"
+  instance_type = "t2.micro"
+}
+
+test test test
